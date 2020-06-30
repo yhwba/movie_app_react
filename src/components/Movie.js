02cf9326b57676869619c0d774b255efec9816d0
yhwba <yhwba@naver.com>
2020-06-30 20:26:31 +0900

    Movie_app base finsh

---
+++ src/components/Movie.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({id, year, title, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <Link to ={{
+                pathname: `/movie/${id}`,
+                state : {
+                    year:year ,
+                    title:title,
+                    summary:summary,
+                    poster:poster,
+                    genres:genres
+                }
+            }}>
+                <img src={poster} alt={title} title={title}/>
+                <div className="movie_data">
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <ul className="movie_genres">{genres.map((genres, index) => <li key={index} className="genres_genre">{genres}</li>)}</ul>
+                    <p className="movie_summary">{summary.slice(0, 180)}...</p>
+                </div> 
+            </Link>
+        </div>
+    );
+};
+Movie.prototype = {
+    id : PropTypes.number.isRequired,
+    year:PropTypes.number.isRequired,
+    title:PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
+}
+export default Movie;
