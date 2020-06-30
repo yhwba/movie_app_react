1d973b67ed9679f9bb8a9287439997cbefd0e8b1
yhwba <yhwba@naver.com>
2020-06-30 18:25:58 +0900

    Updates

---
+++ precache-manifest.eba04f84ed4a0090e4cc67bc4d850845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3a8b8d858ecd3185fd332a27aaa9ea",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "abd5781834549158b36f",
+    "url": "/movie_app_react/static/css/main.569251e2.chunk.css"
+  },
+  {
+    "revision": "b465fb3a41db387c5800",
+    "url": "/movie_app_react/static/js/2.6760420d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.6760420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd5781834549158b36f",
+    "url": "/movie_app_react/static/js/main.a0f78fda.chunk.js"
+  },
+  {
+    "revision": "c497ed83440cab9ff482",
+    "url": "/movie_app_react/static/js/runtime-main.72817d1d.js"
+  }
+]);
