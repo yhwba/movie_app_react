cb9a625213064acaefd8d3207a7f74efc68bc5f5
yhwba <yhwba@naver.com>
2020-07-02 12:17:23 +0900

    Updates

---
+++ precache-manifest.14b5fd24b698290f314e5ea885fb1c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d186717c88909f049d6e62499fca7883",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "e457744441329d6df6ac",
+    "url": "/movie_app_react/static/css/main.93e6df8b.chunk.css"
+  },
+  {
+    "revision": "f3deea0e2656a16d9302",
+    "url": "/movie_app_react/static/js/2.2d1d6c1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_react/static/js/2.2d1d6c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e457744441329d6df6ac",
+    "url": "/movie_app_react/static/js/main.adb9cddd.chunk.js"
+  },
+  {
+    "revision": "c497ed83440cab9ff482",
+    "url": "/movie_app_react/static/js/runtime-main.72817d1d.js"
+  },
+  {
+    "revision": "b2c9324e548579316cb147e3af9c88b2",
+    "url": "/movie_app_react/static/media/logo.b2c9324e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_react/precache-manifest.71084af76bb2cea96dbf57047b918817.js"
+  "/movie_app_react/precache-manifest.14b5fd24b698290f314e5ea885fb1c4d.js"
 );
 
 self.addEventListener('message', (event) => {
